6f5bb477eefe3f36390d9c5c1ae9308190b39a72
Lê Hoàng <icy.lord.love.angel@gmail.com>
2015-09-19 18:05:20 +0700

    Add Level control pm8xxx-vibrator.c

--- drivers/misc/pm8xxx-vibrator.c
+++ drivers/misc/pm8xxx-vibrator.c
@@ -20,6 +20,7 @@
 #include <linux/mfd/pm8xxx/core.h>
 #include <linux/mfd/pm8xxx/vibrator.h>
 
+#include "../staging/iio/sysfs.h"
 #include "../staging/android/timed_output.h"
 
 #define FEATURE_LEVEL_CONTROL
@@ -41,6 +42,7 @@ static int DebugON=0;
 #define VIB_MAX_LEVEL_mV	3100
 #define VIB_MIN_LEVEL_mV	1200
 
+static unsigned int vib_level_range=(VIB_MAX_LEVEL_mV - VIB_MIN_LEVEL_mV) / 100;
 struct pm8xxx_vib {
 	struct hrtimer vib_timer;
 	struct timed_output_dev timed_dev;
@@ -254,6 +256,35 @@ static enum hrtimer_restart pm8xxx_vib_timer_func(struct hrtimer *timer)
 	return HRTIMER_NORESTART;
 }
 
+static ssize_t pm8xxx_vib_voltage_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	int level = vib_dev->level ;
+
+	// convert to percentage form
+	level = (level - VIB_MIN_LEVEL_mV/100) * 100 / vib_level_range ; 
+
+	return sprintf(buf, "%d\n", level);
+}
+
+static ssize_t pm8xxx_vib_voltage_store(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t size)
+{
+        int level_multiply;
+	sscanf(buf, "%d", &level_multiply);
+	if (level_multiply > 100)
+		level_multiply = 100;
+	else if (level_multiply < 0)
+		level_multiply = 0;
+
+	vib_dev->level = VIB_MIN_LEVEL_mV/100 + vib_level_range * level_multiply / 100;
+
+	return size;
+}
+
+static struct device_attribute pm8xxx_vib_devie_attrs[] = {
+        __ATTR(amp, S_IWUGO | S_IRUGO, pm8xxx_vib_voltage_show, pm8xxx_vib_voltage_store),
+};
 #ifdef CONFIG_PM
 static int pm8xxx_vib_suspend(struct device *dev)
 {
@@ -281,8 +312,8 @@ static int __devinit pm8xxx_vib_probe(struct platform_device *pdev)
 	u8 val;
 	int rc;
 
+	int i;
 	dbg_func_in ();
-	
 	if (!pdata)
 		return -EINVAL;
 
@@ -334,11 +365,25 @@ static int __devinit pm8xxx_vib_probe(struct platform_device *pdev)
 
 	vib_dev = vib;
 
+	for (i = 0; i < ARRAY_SIZE(pm8xxx_vib_devie_attrs); i++) {
+		rc = device_create_file(vib->dev,
+				&pm8xxx_vib_devie_attrs[i]);
+		if (rc < 0) {
+			pr_err("%s: failed to create sysfs\n", __func__);
+			goto err_sysfs;
+		}
+	}
 	return 0;
 
 	dbg_func_out ();
 err_read_vib:
 	kfree(vib);
+	return rc;
+err_sysfs:
+	for (; i >= 0; i--) {
+		device_remove_file(vib->dev,
+				&pm8xxx_vib_devie_attrs[i]);
+	}
 	return rc;
 }
 
@@ -371,7 +416,6 @@ static int __init pm8xxx_vib_init(void)
 {
 	dbg_func_in();
 	return platform_driver_register(&pm8xxx_vib_driver);
-	
 	dbg_func_out();
 }
 module_init(pm8xxx_vib_init);
