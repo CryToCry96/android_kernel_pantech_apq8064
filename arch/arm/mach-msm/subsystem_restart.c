ab166078c25bb1280804b51a36dcdc586a6edd68
Linh Phi <linhphi9x94@gmail.com>
2016-11-06 11:51:12 +0700

    Revert "ef51: Disable Pantech logging because random reboot"
    This reverts commit 4f7d2eb926739251e58febb65d6ebd0937dfe864.

--- arch/arm/mach-msm/subsystem_restart.c
+++ arch/arm/mach-msm/subsystem_restart.c
@@ -638,9 +638,7 @@ struct excute_work {
 struct excute_work *ssr_noti_work;
 #endif
 
-#ifdef CONFIG_PANTECH_DEBUG
 extern int wifissr;
-#endif
 int subsystem_restart_dev(struct subsys_device *dev)
 {
 	const char *name = dev->desc->name;
@@ -656,12 +654,8 @@ int subsystem_restart_dev(struct subsys_device *dev)
 		return -EBUSY;
 	}
     // wifi ssrmode check
-#ifdef CONFIG_PANTECH_DEBUG
     if( 1 == wifissr &&
         (!strncmp(name, "wcnss", 5) || !strncmp(name, "riva", 4)) )
-#else
-    if (!strncmp(name, "wcnss", 5) || !strncmp(name, "riva", 4))
-#endif
     {
         restart_level = RESET_SUBSYS_INDEPENDENT;
     }
